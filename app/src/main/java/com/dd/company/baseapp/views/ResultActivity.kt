9764ff8e3ea4621bec9cd67de74a4c5f1a77b316
doanvu2000 <dauphong13@gmail.com>
2022-10-25 23:04:18 +0700

    add logic result_screen

--- app/src/main/java/com/dd/company/baseapp/views/ResultActivity.kt
+++ app/src/main/java/com/dd/company/baseapp/views/ResultActivity.kt
@@ -1,14 +1,15 @@
 package com.dd.company.baseapp.views
 
-import android.R.attr.label
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
 import com.dd.company.baseapp.base.BaseActivity
 import com.dd.company.baseapp.base.RESULT
 import com.dd.company.baseapp.databinding.ActivityResultBinding
+import com.dd.company.baseapp.extensions.setOnSafeClick
 
 
 class ResultActivity : BaseActivity<ActivityResultBinding>() {
@@ -21,21 +22,33 @@ class ResultActivity : BaseActivity<ActivityResultBinding>() {
 
     override fun initListener() {
         binding.apply {
-            btnCopy.setOnClickListener {
+            btnCopy.setOnSafeClick {
                 val clipboard: ClipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
                 val clip = ClipData.newPlainText(RESULT, result)
                 clipboard.setPrimaryClip(clip)
             }
-            btnOpenBrowser.setOnClickListener {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(result)))
-            }
-            btnShare.setOnClickListener {
-                val intent = Intent(Intent.ACTION_SEND)
-                intent.type = "text/plain"
-                intent.putExtra(Intent.EXTRA_SUBJECT, "Result QR")
-                intent.putExtra(Intent.EXTRA_TEXT, result)
-                startActivity(Intent.createChooser(intent, "Share result QR"))
-            }
+            tvOpenBrowser.setOnSafeClick { openBrowser() }
+            btnOpenBrowser.setOnSafeClick { openBrowser() }
+            btnShare.setOnSafeClick { shareData() }
+            tvShare.setOnSafeClick { shareData() }
+            btnBack.setOnSafeClick { onBackPressed() }
+        }
+    }
+
+    private fun shareData() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Result QR")
+        intent.putExtra(Intent.EXTRA_TEXT, result)
+        startActivity(Intent.createChooser(intent, "Share result QR"))
+    }
+
+    private fun openBrowser() {
+        try {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(result)))
+        } catch (e: Exception) {
+            Toast.makeText(this@ResultActivity, "Cannot open browser with $result", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
         }
     }
 
